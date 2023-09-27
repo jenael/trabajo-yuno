dd6fdce3d81767b148839e07776b215719a96e75
Jenael <jenselrafaelgarciaaleman3@gmail.com>
2023-09-27 15:00:12 -0600

    Menu

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int opcion = 0;
+        do
+        {
+            Console.Clear(); 
+            Console.WriteLine("menú de operaciones:");
+            Console.WriteLine("1. Pares e Impares");
+            Console.WriteLine("2. Tabla de multiplicar");
+            Console.WriteLine("3. Factorial de un número");
+            Console.WriteLine("4. Números Primos");
+            Console.WriteLine("5. Sucesión de Fibonacci");
+            Console.WriteLine("6. Salir");
+            Console.Write("Ingrese una opción del 1 al 6: ");
+
+            string? input = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(input) && int.TryParse(input, out opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        ParesImpares();
+                        break;
+                    case 2:
+                        //TablaDeMultiplicar();
+                        break;
+                    case 3:
+                        //Factorial();
+                        break;
+                    case 4:
+                        //NumerosPrimos();
+                        break;
+                    case 5:
+                        //Fibonacci();
+                        break;
+                    case 6:
+                        Console.WriteLine("¡Adios...!");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada no válida. ingrese un número del 1 al 6.");
+            }
+
+        } while (opcion != 6); 
+    }
+        static void ParesImpares()
+    {
+        do
+        {
+            Console.Clear(); 
+            Console.Write("Ingrese un número entero positivo para realizar las operaciones de pares e impares: ");
+            string? input = Console.ReadLine();
+            if (!string.IsNullOrEmpty(input) && int.TryParse(input, out int numero))
+            {
+                Console.WriteLine("Pares: ");
+                for (int i = 0; i <= numero; i++)
+                {
+                    if (i % 2 == 0)
+                        Console.Write(i + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Impares: ");
+                for (int i = 0; i <= numero; i++)
+                {
+                    if (i % 2 != 0)
+                        Console.Write(i + " ");
+                }
+                Console.WriteLine();
+
+                int sumaPares = Enumerable.Range(0, numero + 1).Where(x => x % 2 == 0).Sum();
+                int sumaImpares = Enumerable.Range(0, numero + 1).Where(x => x % 2 != 0).Sum();
+                Console.WriteLine($"Suma de pares: {sumaPares}");
+                Console.WriteLine($"Suma de impares: {sumaImpares}");
+                Console.WriteLine(sumaPares > sumaImpares ? "La suma de pares es mayor que la suma de impares." : "La suma de impares es mayor que la suma de pares.");
+            }
+            else
+            {
+                Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero positivo.");
+            }
+
+            Console.Write("¿Desea realizar otra operación en Pares e Impares? (s/n): ");
+        } while (Console.ReadLine()?.Trim().ToLower() == "s");
+
+        Console.Clear(); 
+        
+    
